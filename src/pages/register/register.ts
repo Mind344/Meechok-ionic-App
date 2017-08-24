6d2e3a5febedde888da6a25ab13d7edd7592d257
mind344 <teemaporn.344@gmail.com>
2017-08-24 12:00:19 +0700

    Update register.ts

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the RegisterPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { ActionSheetController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,11 +10,65 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public actionSheetCtrl: ActionSheetController, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+  proPic() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'เพิ่มรูปโปรไฟล์',
+      buttons: [
+        {
+          text: 'เลือกรูปจากคลัง',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        }, {
+          text: 'ถ่ายรูป',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        }, {
+          text: 'ยกเลิก',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+  confirm() {
+    let alert = this.alertCtrl.create({
+      // if(condition) {
+        title: '<center style="font-family: \'bangna\';">\
+              <img src="img/smile1.png" width="50">\n \
+              </center>\
+              ',
+          subTitle: ' <center>ยินดีต้อนรับสมาชิก<br> \
+                  <h3>มีโชคพลาซ่า</h3>\
+                  </center>\
+                ',
+            buttons: ['ตกลง']
+      // }else{
+
+        // title: '<center style="font-family: \'bangna\';">\
+        //         <img src="img/sad1.png" width="50">\n \
+        //         </center>\
+        //         ',
+        // subTitle: ' <center>บันทึกข้อมูลไม่สำเร็จ<br> \
+        //         กรุณาตรวจสอบข้อมูลอีกครั้ง\
+        //         </center>\
+        //   ',
+        // buttons: ['ตกลง','ยกเลิก']
+      
+
+    });
+    alert.present();
   }
 
 }
